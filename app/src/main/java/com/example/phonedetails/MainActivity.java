457ee94cee09a69ba5c97bab10661de5694515fe
sedali <seyedalian@outlook.com>
2019-12-25 21:04:45 +0330

    main activity rebuilding

--- app/src/main/java/com/example/phonedetails/MainActivity.java
+++ app/src/main/java/com/example/phonedetails/MainActivity.java
@@ -2,41 +2,19 @@ package com.example.phonedetails;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.CellInfo;
-import android.telephony.CellInfoGsm;
-import android.telephony.CellInfoLte;
-import android.telephony.CellInfoWcdma;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "main";
-    Button btnStart;
-    Button btnClean;
+    private static final String TAG = "MainActivity";
     Button btnMap;
-    TextView tvInfo;
-    String info = "";
-    TelephonyManager manager;
-    LocationListener locationListener;
-    LocationManager locationManager;
 
 
 
@@ -45,31 +23,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvInfo = findViewById(R.id.tv_info);
-        btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View view) {
-                clean();
-                MyTelephonyManager();
-            }
-        });
-        btnClean = findViewById(R.id.btnClean);
-        btnClean.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clean();
-            }
-        });
+
         btnMap = findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MapsActivity.class));
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
-        manager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
     @Override
@@ -77,152 +38,53 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         check_READ_PHONE_STATE_permission();
     }
-
-    private void check_ACCESS_LOCATION_permissions() {
-        if(ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            Log.d(TAG, "permissions: Location permission requested.");
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION},200);
-        }
-    }
-
     private void check_READ_PHONE_STATE_permission() {
-        if(ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE)!=PackageManager.PERMISSION_GRANTED){
-            Log.d(TAG, "permissions: READ_PHONE_STATE permission requested.");
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},100);
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE)!=PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "permissions:request READ_PHONE_STATE permission.");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE},100);
+        }
+    }
+
+    private void check_LOCATION_permissions() {
+        if (android.support.v4.app.ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+                &&
+                android.support.v4.app.ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+
+            Log.d(TAG, "permissions:request Location permissions.");
+            android.support.v4.app.ActivityCompat.requestPermissions(this,
+                    new String[]
+                            {
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION},
+                    200);
         }
     }
 
 
-    @SuppressLint("ShowToast")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case (100): //Read Phone State
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        switch (requestCode) {
+            case (100): //READ_PHONE_STATE
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(TAG, "onRequestPermissionsResult: READ_PHONE_STATE permission granted.");
-                    check_ACCESS_LOCATION_permissions();
-                }else {
+                    check_LOCATION_permissions();
+
+                } else {
                     Log.d(TAG, "onRequestPermissionsResult: READ_PHONE_STATE permission denied.finish.");
                     finish();
                 }
                 break;
-            case (200)://Access Location
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                        grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                    Log.d(TAG, "onRequestPermissionsResult: Access Location permissions granted.");
-                }else {
+            case (200)://ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: ACCESS_FINE_LOCATION permissions granted.");
+                } else {
                     Log.d(TAG, "onRequestPermissionsResult: Access Location permissions denied.finish.");
                     finish();
                 }
                 break;
-
-        }
-    }
-
-
-
-    @TargetApi(Build.VERSION_CODES.P)
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void MyTelephonyManager() {
-//        Log.d(TAG, "MyTelephonyManager");
-//        int phoneType = manager.getPhoneType();
-//        switch (phoneType) { //phoneType indicates the type of radio used to transmit voice calls.
-//            case (TelephonyManager.PHONE_TYPE_CDMA):
-//                strPhoneType = "CDMA";
-//                break;
-//            case (TelephonyManager.PHONE_TYPE_GSM):
-//                strPhoneType = "GSM";
-//                break;
-//            case (TelephonyManager.PHONE_TYPE_SIP):
-//                strPhoneType = "SIP";
-//                break;
-//            case (TelephonyManager.PHONE_TYPE_NONE):
-//                strPhoneType = "NONE";
-//                break;
-//        }
-
-//        boolean isRoaming = manager.isNetworkRoaming();
-//        String PhoneType = strPhoneType;
-//        @SuppressLint("MissingPermission") String imei = manager.getImei();//International Mobile Equipment Identity (IMEI)
-//        @SuppressLint({"HardwareIds", "MissingPermission"}) String subscriberId = manager.getSubscriberId(); //IMSI
-//        @SuppressLint({"HardwareIds", "MissingPermission"}) String simSerialNumber = manager.getSimSerialNumber();
-//        String networkCountryIso = manager.getNetworkCountryIso();
-//        String simCountryIso = manager.getSimCountryIso();
-        String networkOperatorName = manager.getNetworkOperatorName();
-        String networkOperator = manager.getNetworkOperator();
-        info += "network Operator : " + networkOperator;
-        info += "\nnetwork Operator Name: " + networkOperatorName;
-//        info += "\nPhone Network Type: " + PhoneType;
-//        info += "\nIMEI: " + imei;
-//        info += "\nIMSI: " + subscriberId;
-//        info += "\nSIM Serial Number: " + simSerialNumber;
-//        info += "\nNetwork Country ISO: " + networkCountryIso;
-//        info += "\nSIM Country ISO: " + simCountryIso;
-//        info += "\nRoaming: " + isRoaming;
-
-        @SuppressLint("MissingPermission") List<CellInfo> allCellInfo = manager.getAllCellInfo();
-        info += "\n\nAllCellInfo:\n\n";
-        int count2G = 0;
-        int count3G = 0;
-        int count4G = 0;
-        int sumSS2G = 0;
-        int sumSS3G = 0;
-        int sumSS4G = 0;
-        int avgSS2G;
-        int avgSS3G;
-        int avgSS4G;
-
-        for (int i = 0; i < allCellInfo.size(); ++i) {
-            CellInfo cellInfo = allCellInfo.get(i);
-            if (cellInfo instanceof CellInfoGsm){ //if GSM connection(2G)
-                count2G++;
-                int SS2GdBm = ((CellInfoGsm) cellInfo).getCellSignalStrength().getDbm();
-                sumSS2G += SS2GdBm;
-            }
-            else if(cellInfo instanceof CellInfoLte){  //if LTE connection(4G)
-                count4G++;
-                int SS4GdBm = ((CellInfoLte) cellInfo).getCellSignalStrength().getDbm();
-                sumSS4G += SS4GdBm;
-            }
-            else if (cellInfo instanceof CellInfoWcdma){  //if wcdma connection(3G)
-                count3G++;
-                int SS3GdBm = ((CellInfoWcdma) cellInfo).getCellSignalStrength().getDbm();
-                sumSS4G += SS3GdBm;
-            }
-        }
-        if(count2G != 0){
-            avgSS2G = sumSS2G / count2G;
-            info += "\navgSS2G = " + avgSS2G;
-        }
-        if(count3G != 0){
-            avgSS3G = sumSS3G / count3G;
-            info += "\navgSS3G = " + avgSS3G;
-        }
-        if(count4G != 0){
-            avgSS4G = sumSS4G / count4G;
-            info += "\navgSS4G = " + avgSS4G;
-        }
-
-        tvInfo.setText(info);
-    }
-
-    public void clean() {
-        info = "";
-        tvInfo.setText("");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(locationManager != null){
-            locationManager.removeUpdates(locationListener);
         }
     }
 }
--- app/src/main/java/com/example/phonedetails/MapsActivity.java
+++ app/src/main/java/com/example/phonedetails/MapsActivity.java
@@ -1,12 +1,21 @@
 package com.example.phonedetails;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +32,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.PolylineOptions;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
@@ -34,12 +43,14 @@ public class MapsActivity extends FragmentActivity implements
         GoogleMap.OnMapLongClickListener,
         GoogleMap.OnMarkerClickListener {
 
-    private static final String TAG = "mapp";
+    private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
     private GoogleApiClient googleApiClient;
     private Location lastLocation;
-    Button btnStartStop;
-    String startStopStr = "start";
+    private Button btnStartStop;
+    private boolean isStart = false;
+    private TelephonyManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +63,12 @@ public class MapsActivity extends FragmentActivity implements
             mapFragment.getMapAsync(this);
         }
     }
+
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if(googleApiClient == null){
+        if (googleApiClient == null) {
             buildGoogleApiClient();
         }
         mMap.setMyLocationEnabled(true);
@@ -64,28 +76,52 @@ public class MapsActivity extends FragmentActivity implements
         btnStartStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startStop();
+                startStopFunction();
             }
         });
+        manager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
 //        drawPreviousSignalStrengthCirclesFromDB();
     }
 
     @SuppressLint("SetTextI18n")
-    private void startStop() {
-        if (startStopStr.equals("start")) {
-            startStopStr = "stop";
+    private void startStopFunction() {
+        if (isStart) {
+            isStart = false;
             btnStartStop.setText("Stop");
-            btnStartStop.setBackgroundColor(Color.parseColor("#D82B1B"));
-//            addSignalStrengthCircle(5,
-//                    new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude()));
-        }else if(startStopStr.equals("stop")){
-            startStopStr = "start";
-            btnStartStop.setText("Start");
-            btnStartStop.setBackgroundColor(Color.parseColor("#359c5e"));
+            btnStartStop.setTextColor(Color.parseColor("#D82B1B"));
+            @SuppressLint("MissingPermission") List<CellInfo> allCellInfo = manager.getAllCellInfo();
+            LatLng latLng = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+            int maxSS2G=-200,maxSS3G=-200,maxSS4G=-200;
+            for (int i = 0; i < allCellInfo.size(); ++i) {
+                CellInfo cellInfo = allCellInfo.get(i);
+                if (cellInfo instanceof CellInfoGsm){ //if GSM connection(2G)
+                    int cellid = ((CellInfoGsm)cellInfo).getCellIdentity().getCid();
+                    CellSignalStrengthGsm cellss = ((CellInfoGsm)cellInfo).getCellSignalStrength();
+                    int SS2GdBm = cellss.getDbm();
+                    if(SS2GdBm > maxSS2G) maxSS2G = SS2GdBm;
+                }
+                else if(cellInfo instanceof CellInfoLte){  //if LTE connection(4G)
+                    int cellid = ((CellInfoLte)cellInfo).getCellIdentity().getCi();
+                    CellSignalStrengthLte cellss = ((CellInfoLte)cellInfo).getCellSignalStrength();
+                    int SS4GdBm = cellss.getDbm();
+                    if(SS4GdBm > maxSS4G) maxSS4G = SS4GdBm;
+                }
+                else if (cellInfo instanceof CellInfoWcdma){  //if wcdma connection(3G)
+                    int cellid = ((CellInfoWcdma)cellInfo).getCellIdentity().getCid();
+                    CellSignalStrengthWcdma cellss = ((CellInfoWcdma)cellInfo).getCellSignalStrength();
+                    int SS3GdBm = cellss.getDbm();
+                    if(SS3GdBm > maxSS3G) maxSS3G = SS3GdBm;
+                }
+            }
+            Log.d(TAG, "startStopFunction: maxSS26="+maxSS2G);
+            Log.d(TAG, "startStopFunction: maxSS36="+maxSS3G);
+            Log.d(TAG, "startStopFunction: maxSS46="+maxSS4G);
         }else{
-            Log.e(TAG, "startStop: invalid startStopStr");
+            isStart = true;
+            btnStartStop.setText("Start");
+            btnStartStop.setTextColor(Color.parseColor("#359c5e"));
+
         }
-        Log.d(TAG, "startStop: startStopStr="+startStopStr);
     }
     private void addSignalStrengthCircle(int signalLevel,LatLng latLng) {
         int radius=30,alpha=120,red=255,green=255,blue=255;
@@ -130,8 +166,9 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onLocationChanged(Location location) {
         Log.d(TAG, "onLocationChanged");
-        if(lastLocation!=null)
-        Log.d(TAG, " --> lastLocation: "+lastLocation.getLatitude()+"/"+lastLocation.getLongitude());
+        if(lastLocation!=null){
+            Log.d(TAG, " --> lastLocation: "+lastLocation.getLatitude()+"/"+lastLocation.getLongitude());
+        }
         Log.d(TAG, " --> location: "+location.getLatitude()+"/"+location.getLongitude());
         lastLocation = location;
         LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
@@ -140,19 +177,57 @@ public class MapsActivity extends FragmentActivity implements
         if(googleApiClient != null){
             LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,this);
         }
-        if(startStopStr.equals("stop")){
-            double lng = location.getLongitude();
-            double lat = location.getLatitude();
-            int AvgSS;
-            String Opr;
-            String Gen;
-            //save to db...
-            //draw ss circle
-            //obtain signal level from AvgSS
-            int signalLevel=5;
-            addSignalStrengthCircle(signalLevel,latLng);
+        if(!isStart){
+            @SuppressLint("MissingPermission") List<CellInfo> allCellInfo = manager.getAllCellInfo();
+            int maxSS2G=-200,maxSS3G=-200,maxSS4G=-200;
+            for (int i = 0; i < allCellInfo.size(); ++i) {
+                CellInfo cellInfo = allCellInfo.get(i);
+                if (cellInfo instanceof CellInfoGsm){ //if GSM connection(2G)
+                    int cellid = ((CellInfoGsm)cellInfo).getCellIdentity().getCid();
+                    CellSignalStrengthGsm cellss = ((CellInfoGsm)cellInfo).getCellSignalStrength();
+                    int SS2GdBm = cellss.getDbm();
+                    if(SS2GdBm > maxSS2G) maxSS2G = SS2GdBm;
+                }
+                else if(cellInfo instanceof CellInfoLte){  //if LTE connection(4G)
+                    int cellid = ((CellInfoLte)cellInfo).getCellIdentity().getCi();
+                    CellSignalStrengthLte cellss = ((CellInfoLte)cellInfo).getCellSignalStrength();
+                    int SS4GdBm = cellss.getDbm();
+                    if(SS4GdBm > maxSS4G) maxSS4G = SS4GdBm;
+                }
+                else if (cellInfo instanceof CellInfoWcdma){  //if wcdma connection(3G)
+                    int cellid = ((CellInfoWcdma)cellInfo).getCellIdentity().getCid();
+                    CellSignalStrengthWcdma cellss = ((CellInfoWcdma)cellInfo).getCellSignalStrength();
+                    int SS3GdBm = cellss.getDbm();
+                    if(SS3GdBm > maxSS3G) maxSS3G = SS3GdBm;
+                }
+            }
+            Log.d(TAG, "startStopFunction: maxSS26="+maxSS2G);
+            Log.d(TAG, "startStopFunction: maxSS36="+maxSS3G);
+            Log.d(TAG, "startStopFunction: maxSS46="+maxSS4G);
+
+            int signalLevel2G = dBmToSignalLevel(maxSS4G);
+            //addNewRecordToDB(lng,lat,signalLevel,avgSs,opr,gen);
+            addSignalStrengthCircle(signalLevel2G,latLng);
+//            int signalLevel3G = dBmToSignalLevel(maxSS3G);
+//            //addNewRecordToDB(lng,lat,signalLevel,avgSs,opr,gen);
+//            addSignalStrengthCircle(signalLevel3G,latLng);
+//            int signalLevel4G = dBmToSignalLevel(maxSS4G);
+//            //addNewRecordToDB(lng,lat,signalLevel,avgSs,opr,gen);
+//            addSignalStrengthCircle(signalLevel4G,latLng);
         }
     }
+
+    private int dBmToSignalLevel(int dBm) {
+        if(dBm < 0 && dBm >= -55) return 5;
+        if(dBm < -55 && dBm >= -65) return 4;
+        if(dBm < -65 && dBm >= -75) return 3;
+        if(dBm < -75 && dBm >= -85) return 2;
+        if(dBm < -85 && dBm >= -120) return 1;
+        else return 0;
+    }
+
+
+
 
     @SuppressLint("MissingPermission")
     @Override
